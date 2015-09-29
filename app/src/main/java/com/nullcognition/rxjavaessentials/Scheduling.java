471df0fdf7b58da002e315a012fe0ca0d1406a9a
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-09-29 14:49:09 -0400

    templated: chapter07

---
+++ app/src/main/java/com/nullcognition/rxjavaessentials/Scheduling.java
@@ -0,0 +1,20 @@
+package com.nullcognition.rxjavaessentials;
+// ersin 29/09/15 Copyright (c) 2015+ All rights reserved.
+
+
+public class Scheduling{
+
+	// Strict mode
+
+	// avoiding blocking
+
+	// schedulers - io, computation, immediate, newThread, trampoline
+
+	// nonblocking io
+
+	// subscribeOn/ObserveOn
+
+	// long running tasks
+
+	// network tasks
+}
